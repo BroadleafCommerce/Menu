f62e539462ebe28c4e9fb1c98e673088e403d897
Elbert Bautista <ebautista@broadleafcommerce.org>
2014-06-24 10:02:34 -0500

    Need to extend TranslatedEntity and add Menu domain

---
+++ src/main/java/org/broadleafcommerce/menu/domain/MenuTranslatedEntity.java
@@ -0,0 +1,36 @@
+/*
+ * #%L
+ * BroadleafCommerce Menu
+ * %%
+ * Copyright (C) 2009 - 2014 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.broadleafcommerce.menu.domain;
+
+import org.broadleafcommerce.common.i18n.domain.TranslatedEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Elbert Bautista (elbertbautista)
+ */
+@Component("blMenuTranslatedEntity")
+public class MenuTranslatedEntity extends TranslatedEntity {
+
+    private static final long serialVersionUID = -1;
+
+    public static final TranslatedEntity MENU = new TranslatedEntity("org.broadleafcommerce.menu.domain.Menu", "Menu");
+    public static final TranslatedEntity MENU_ITEM = new TranslatedEntity("org.broadleafcommerce.menu.domain.MenuItem", "MenuItem");
+
+}
