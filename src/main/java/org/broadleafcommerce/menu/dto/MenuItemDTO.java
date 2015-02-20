df42433420669f85fa7035a52216cd14c18b21fd
Gonzalo Diaz <gdiaz@broadleafcommerce.com>
2015-02-20 16:35:27 -0600

    added a "category ID" property, and populated it if the menu item in question refers to  category

--- src/main/java/org/broadleafcommerce/menu/dto/MenuItemDTO.java
+++ src/main/java/org/broadleafcommerce/menu/dto/MenuItemDTO.java
@@ -38,9 +38,14 @@ public class MenuItemDTO implements Serializable {
     protected String url;
     protected String imageUrl;
     protected String altText;
-    protected List<MenuItemDTO> submenu = new ArrayList<MenuItemDTO>();
-
-    public String getLabel() {
+    /**
+     * not always set, only it the menu item represents a category.
+     */
+    protected Long categoryId;
+    
+	protected List<MenuItemDTO> submenu = new ArrayList<MenuItemDTO>();
+  
+	public String getLabel() {
         return label;
     }
 
@@ -79,5 +84,18 @@ public class MenuItemDTO implements Serializable {
     public void setSubmenu(List<MenuItemDTO> submenu) {
         this.submenu = submenu;
     }
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+  
 
 }
