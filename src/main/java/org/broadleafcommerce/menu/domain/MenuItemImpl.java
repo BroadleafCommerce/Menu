d78edbda998918c58d022fc3677965add15077ca
Jonathan Fleschler <jon.fleschler@gmail.com>
2015-07-31 13:50:01 -0500

    BroadleafCommerce/QA#791 - Fixed import statements

--- src/main/java/org/broadleafcommerce/menu/domain/MenuItemImpl.java
+++ src/main/java/org/broadleafcommerce/menu/domain/MenuItemImpl.java
@@ -49,7 +49,20 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKey;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
--- src/main/java/org/broadleafcommerce/menu/domain/MenuItemMediaXrefImpl.java
+++ src/main/java/org/broadleafcommerce/menu/domain/MenuItemMediaXrefImpl.java
@@ -38,7 +38,16 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
